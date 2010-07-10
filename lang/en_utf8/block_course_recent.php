9d1a070f2c95e1dcd5fd249e85f21749876201a6
Justin Filip <jfilip@remote-learner.ca>
2010-07-09 20:07:13 -0400

    UCSFCB-3 Rearranged the language strings.

--- lang/en_utf8/block_course_recent.php
+++ lang/en_utf8/block_course_recent.php
@@ -1,16 +1,19 @@
-<?php // $Id: $
-$string['course_recent'] = 'Recent Courses';
+<?php
+
 $string['blockname'] = 'Recent Courses';
+$string['breadcrumb'] = 'Recent Courses user settings';
+$string['course_recent'] = 'Recent Courses';
+$string['course_recent:changelimit'] = 'Change the maximum number of courses to display';
 $string['default_max'] = 'Max number of courses';
 $string['default_max_desc'] = 'The maximum number of recently visited courses to the block will display by default.'.
                               '  This can be overwritten by the user if they have the capability to do so.';
-$string['musthaverole'] = 'User must have a role in the course';
-$string['musthaverole_desc'] = 'Check this if the user must have a role to view a course in the recently visited courses';
-$string['breadcrumb'] = 'Recent Courses user settings';
-$string['settings'] = 'User settings';
-$string['userlimit'] = 'Maximum number of courses to display';
 $string['error1'] = 'The number cannot be less than 1';
 $string['error2'] = 'The number cannot be greater than 10';
 $string['header'] = 'Recent Courses';
-$string['course_recent:changelimit'] = 'Change the maximum number of courses to display';
+$string['musthaverole'] = 'User must have a role in the course';
+$string['musthaverole_desc'] = 'Check this if the user must have a role to view a course in the recently visited courses';
+
+$string['settings'] = 'User settings';
+$string['userlimit'] = 'Maximum number of courses to display';
+
 ?>
