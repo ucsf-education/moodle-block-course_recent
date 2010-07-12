3fcdcda4d709a2a5004a7b81702e2e40795e41e3
Justin Filip <jfilip@remote-learner.ca>
2010-07-12 13:09:26 -0400

    UCSFCB-3 Changed the textfield input for the 'Max number of courses' setting to be a drop-down select instead.

--- settings.php
+++ settings.php
@@ -21,8 +21,14 @@
  */
 
 
-$settings->add(new admin_setting_configtext('block_course_recent_default', get_string('default_max', 'block_course_recent'),
-                   get_string('default_max_desc', 'block_course_recent'), 5, PARAM_INT));
+$choices = array();
+
+for ($i = 1; $i <= 10; $i++) {
+    $choices[$i] = $i;
+}
+
+$settings->add(new admin_setting_configselect('block_course_recent_default', get_string('default_max', 'block_course_recent'),
+                   get_string('default_max_desc', 'block_course_recent'), 5, $choices));
 
 $settings->add(new admin_setting_configcheckbox('block_course_recent_musthaverole', get_string('musthaverole', 'block_course_recent'),
                    get_string('musthaverole_desc', 'block_course_recent'), 1));
