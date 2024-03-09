33dec957f6bf7914ca79f37fde66bfcf33a8e39a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-03-09 15:35:16 -0800

    Bumps version, and required Moodle baseline version to M4.3

--- version.php
+++ version.php
@@ -6,6 +6,6 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022121200;
-$plugin->requires  = 2022111800;
+$plugin->version   = 2024030900;
+$plugin->requires  = 2023100400;
 $plugin->component = 'block_course_recent';
